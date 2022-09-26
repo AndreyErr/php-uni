5b082b69d11dcb46e48f5b4dbcbaac283519a507
Sorokin Andrey <a9165185808@gmail.com>
2022-09-26 23:43:00 +0300

    ПР2

--- web/index.php
+++ web/index.php
@@ -4,6 +4,9 @@
     <link rel="stylesheet" href="style.css" type="text/css"/>
 </head>
 <body>
+    <a href="http://localhost:9000/Drawer.php?num=333">Drawer</a>
+    <a href="http://localhost:9000/Sort.php?array=6,8,1,2,5,1,3,5,4,7,9,4">Sort</a>
+    <a href="http://localhost:9000/Unix.php">Unix</a>
 <h1>Таблица пользователей данного продукта</h1>
 <table>
     <tr><th>Id</th><th>Name</th><th>Surname</th></tr>
